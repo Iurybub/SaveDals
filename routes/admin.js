7526bdd65143f0c3f7e417314df16723e2f5e6a3
IuryB <iurybubnov16@gmail.com>
2021-04-15 21:13:11 -0700

    Route Protection

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,19 @@
 const express = require("express");
-
 const adminController = require("../controllers/admin");
 
 const router = express.Router();
+const ensureAuth = require("../middleware/ensureAuth");
 
-router.get("/", adminController.getDashboard); //Done
+router.get("/", ensureAuth, adminController.getDashboard); //Done
 
-router.get("/animals", adminController.getAnimals); //Done
-router.get("/animals/details/:id", adminController.getAnimalDetails);
-router.get("/animals/add-new", adminController.getAddNew);
-router.post("/animals/add-new-post", adminController.postAddNew);
+router.get("/animals", ensureAuth, adminController.getAnimals); //Done
+router.get(
+  "/animals/details/:id",
+  ensureAuth,
+  adminController.getAnimalDetails
+);
+router.get("/animals/add-new", ensureAuth, adminController.getAddNew);
+router.post("/animals/add-new-post", ensureAuth, adminController.postAddNew);
 // router.get("/animals/pending", adminController.getPending);
 // router.post("/animals/pending/:id", adminController.postPending);
 // router.get("/animals/questions", adminController.getQuestions);
